a269d8a6b1dcd6808db50462e74b5586547e0704
Jinzhu <wosmvp@gmail.com>
2017-02-09 16:52:05 +0800

    Refactor filebox controller, fix #1

--- controller.go
+++ controller.go
@@ -13,21 +13,28 @@ import (
 
 // Download is a handler will return a specific file
 func (filebox *Filebox) Download(w http.ResponseWriter, req *http.Request) {
-	filePath := strings.TrimPrefix(req.URL.Path, filebox.prefix)
-	context := &admin.Context{Context: &qor.Context{Request: req, Writer: w}}
-	matchedRoles := roles.MatchedRoles(req, filebox.Auth.GetCurrentUser(context))
+	var (
+		currentUser qor.CurrentUser
+		filePath    = strings.TrimPrefix(req.URL.Path, filebox.prefix)
+		context     = &admin.Context{Context: &qor.Context{Request: req, Writer: w}}
+	)
+
+	if auth := filebox.Auth; auth != nil {
+		currentUser = auth.GetCurrentUser(context)
+	}
+
+	matchedRoles := roles.MatchedRoles(req, currentUser)
 
 	file := filebox.AccessFile(filePath, matchedRoles...)
 	if reader, err := file.Read(); err == nil {
 		fileName := filepath.Base(file.FilePath)
+
 		w.Header().Set("Content-Disposition", "attachment; filename="+fileName)
 		w.Header().Set("Content-Type", req.Header.Get("Content-Type"))
 		http.ServeContent(w, req, fileName, time.Now(), reader)
-		return
 	} else if err == roles.ErrPermissionDenied && filebox.Auth != nil {
 		http.Redirect(w, req, filebox.Auth.LoginURL(context), http.StatusFound)
-		return
+	} else {
+		http.NotFound(w, req)
 	}
-
-	http.NotFound(w, req)
 }
